15496f5addd19f326a38ebc3b6345b55297be238
Bernardo Amorim <bsandinoamorim@gmail.com>
2017-07-10 12:41:20 -0300

    Fix minor bug

--- src/run_config.sh
+++ src/run_config.sh
@@ -11,37 +11,37 @@ done
 
 if [ "$NAME" == "full" ]
 then  
-    $x1=-2.500
-    $x2=1.500
-    $y1=-2.000
-    $y2=2.000
+    x1=-2.500
+    x2=1.500
+    y1=-2.000
+    y2=2.000
 elif [ "$NAME" == "seahorse" ]
 then
-    $x1=-0.800
-    $x2=-0.700
-    $y1=0.050
-    $y2=0.150
+    x1=-0.800
+    x2=-0.700
+    y1=0.050
+    y2=0.150
 elif [ "$NAME" == "elephant" ]
 then
-    $x1=0.175
-    $x2=0.375
-    $x3=-0.100
-    $x4=0.100
+    x1=0.175
+    x2=0.375
+    x3=-0.100
+    x4=0.100
 elif [ "$NAME" == "triple_spiral" ]
 then
-    $x1=-0.188
-    $x2=-0.012
-    $y1=0.554
-    $y2=0.754
+    x1=-0.188
+    x2=-0.012
+    y1=0.554
+    y2=0.754
 fi
 LOG_NAME=$NAME".log"
-perf stat -r $N_MEASUREMENTS -d ./mandelbrot_seq $x1  $x2 $y1 $y2 $SIZE >> $LOG_NAME 2>&1
+perf stat -r $N_MEASUREMENTS -d ./mandelbrot_seq $x1 $x2 $y1 $y2 $SIZE >> $LOG_NAME 2>&1
 mv output.ppm pictures/$NAME"_seq.ppm"
 
-perf stat -r $N_MEASUREMENTS -d mpirun -host $HOST_STR ./mandelbrot_mpi $x1  $x2 $y1 $y2 $SIZE >> $LOG_NAME 2>&1
+perf stat -r $N_MEASUREMENTS -d mpirun -host $HOST_STR ./mandelbrot_mpi $x1 $x2 $y1 $y2 $SIZE >> $LOG_NAME 2>&1
 mv output.ppm pictures/$NAME"_mpi.ppm"
 
-perf stat -r $N_MEASUREMENTS -d mpirun -host $HOST_STR ./mandelbrot_mpi_omp $x1  $x2 $y1 $y2 $N_CORES $SIZE >> $LOG_NAME 2>&1
+perf stat -r $N_MEASUREMENTS -d mpirun -host $HOST_STR ./mandelbrot_mpi_omp $x1 $x2 $y1 $y2 $N_CORES $SIZE >> $LOG_NAME 2>&1
 mv output.ppm pictures/$NAME"_mpi_omp.ppm"
 
 DIFF=$(diff pictures/$NAME"_seq.ppm" pictures/$NAME"_mpi.ppm") 
